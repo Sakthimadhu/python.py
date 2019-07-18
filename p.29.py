fef6b13de99c0ad27e7bbb911e011af34d67d02c
Sakthimadhu <51222816+Sakthimadhu@users.noreply.github.com>
2019-07-18 02:47:34 -0700

    Update p.29.py

--- p.29.py
+++ p.29.py
@@ -1,7 +1,6 @@
-z=input() 
-b=z.replace(".","",1)
-print(b)
-if(b.isdigit()):
-  print("yes") 
-else: 
-  print("No")  
+m=input()
+e=m.lstrip('-').replace('.','',1).isdigit()
+if(e==True):
+	print("yes")
+else:
+	print("No")
